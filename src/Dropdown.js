996dd4150ad3ea2bbd7e6d3012ae68a75599789d
jadsafah <jadsafah@gmail.com>
2020-08-02 19:57:05 -0400

    delete calculate button and edited UI

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -4,28 +4,37 @@ const Dropdown = (props) => {
 
     //state of dropdown toggle and value selected from list
     const [open, setOpen] = useState(false);
-    const [selected, setSelected] = useState("");
+    //const [selected, setSelected] = useState("");
 
     //handle button click to dropdown the list and to extract clicked value from list
     const handleDropdownClick = () => setOpen(!open);
 
     return (
-        <div className="dd-wrapper">
-            <div className="dd-header" onClick={() => handleDropdownClick()}>
-                <div className="dd-header-title">{selected}</div>
-            </div> 
-            {open &&
-                <ul className="dd-list">
-                    {props.items.map(item => 
-                        <li 
-                            className="dd-list-items"
-                            key={item.id}
-                            onClick={() => {props.handleSelectionClick(item, props.type); handleDropdownClick(); setSelected(item.text);}}>
-                            {item.text}
-                        </li>
-                    )}
-                </ul>
-            }
+        <div className="dd-wrapper ">
+            <div className="dd-title">{props.title}</div>
+                <div className="dd-header" onClick={() => handleDropdownClick()}>
+                    <div className="dd-header-title">
+                        <p>{props.state.text}</p>
+                    </div>
+                    <div className="dd-header-arrow">{open
+                        ? <i className="arrow up"></i> : <i className="arrow down"></i>
+                        }
+                    </div>
+                </div>
+                {open &&
+                <div className="dd-list-wrapper">
+                    <ul className="dd-list shadow">
+                        {props.items.map(item => 
+                            <li 
+                                className="dd-list-items"
+                                key={item.id}
+                                onClick={() => {props.handleSelectionClick(item, props.type); handleDropdownClick();}}>
+                                {item.text}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                }
         </div>
     )
 }
